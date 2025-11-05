229e5ab9d0a756dd178af9de99e2dcd422efcf24
Md. Rahim Uddin <rahimuddinduet90@gmail.com>
2025-11-05 22:51:41 +0600

    adding user model and serializer

---
+++ user/Serializer/user_serializer.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers 
+from user.models import User 
+
+
+# User Sign Up Serializer 
+
+
+class UserSignUpSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'password', 'first_name', 'last_name', 'image']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
--- user/models.py
+++ user/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from user.custom_manager import CustomUserManager
+from django.contrib.auth.models import  AbstractUser 
+from django.utils.translation import gettext_lazy as _  
+
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+    image = models.ImageField(upload_to='user_images/', null=True, blank=True) 
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
