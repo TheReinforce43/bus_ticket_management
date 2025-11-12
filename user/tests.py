66ef59011866f78b7d869fc4e28cfdc4f7510e9a
Md. Rahim Uddin <rahimuddinduet90@gmail.com>
2025-11-12 21:54:14 +0600

    modify in tests file

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase , SimpleTestCase 
+from django.urls import reverse, resolve  
 
-# Create your tests here.
+
+class TestUserUrls(SimpleTestCase): 
+
+    def test_user_app_url_is_resolved(self):
+        url = reverse('user-signup')
+
+        print(f"Resolved  Sign Up URL: {url}")  # Debugging line to print the resolved URL
+
+        self.assertEqual(resolve(url).func.view_class.__name__, 'UserSignUpAPIView')
+
+    def test_user_login_url_is_resolved(self):
+        url = reverse('user-login')
+        print(f"Resolved Login URL: {url}")  # Debugging line to print the resolved URL
+
+        self.assertEqual(resolve(url).func.view_class.__name__, 'UserLoginAPIView')
+
+    def test_user_logout_url_is_resolved(self):
+        url = reverse('user-logout')
+        print(f"Resolved Logout URL: {url}")  # Debugging line to print the resolved URL
+        self.assertEqual(resolve(url).func.view_class.__name__, 'UserLogoutAPIView')
+
