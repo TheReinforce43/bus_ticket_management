919c8cfb0cef425f8ada6784d8dc03329fedc789
Md. Rahim Uddin <rahimuddinduet90@gmail.com>
2025-11-21 19:16:34 +0600

    modify the env file

--- bus_ticket_project/settings.py
+++ bus_ticket_project/settings.py
@@ -31,7 +31,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=True)
+
 
 ALLOWED_HOSTS = ['*','bus-ticket-management-won6.onrender.com']
 
