76358c4d92b6859cb0fff6d5e266a0d8d5eba214
Md. Rahim Uddin <rahimuddinduet90@gmail.com>
2025-12-02 23:55:26 +0600

    add : adding redis and celery configuration on the project

--- bus_ticket_project/celery.py
+++ bus_ticket_project/celery.py
@@ -1,2 +1,9 @@
-import os 
-from celery import Celery 
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bus_ticket_project.settings")
+
+app = Celery("bus_ticket_project")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
