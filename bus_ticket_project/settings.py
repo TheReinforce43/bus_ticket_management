a5080633570a6d490afe45591c92075e82c2c19b
Md. Rahim Uddin <rahimuddinduet90@gmail.com>
2025-12-02 23:30:08 +0600

    add : adding redis and celery configuration on the project

--- bus_ticket_project/settings.py
+++ bus_ticket_project/settings.py
@@ -149,6 +149,12 @@ elif DEBUG:
         }
     }
 
+    CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://127.0.0.1:6379/0")
+    CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://127.0.0.1:6379/0")
+    CELERY_ACCEPT_CONTENT = ['json']
+    CELERY_TASK_SERIALIZER = 'json'
+    CELERY_RESULT_SERIALIZER = 'json'
+    CELERY_TIMEZONE = 'Asia/Dhaka'
 else :
     
     DATABASES = {
